81833d03fa185f0b449ea1bb68c34084904dcf9d
Hasan Demirci <200149@studierende.htl-donaustadt.at>
2025-09-29 15:05:04 +0200

    Im boutaaaaaaaaa
    test

--- edge_server/config.py
+++ edge_server/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Settings:
-    PROJECT_NAME: str = "Edge Server"
+    PROJECT_NAME: str = "Edge Server" #edge
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecret")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 day

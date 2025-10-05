e46cc649d506a289dfd9163f23e78074654acd3f
Mohamed Saad <210433@studierende.htl-donaustadt.at>
2025-10-05 22:03:58 +0200

    Login-Logik & SignUp Verbesserung

--- edge_server/database/models.py
+++ edge_server/database/models.py
@@ -8,6 +8,7 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     journeys = relationship("Journey", back_populates="owner")
 
